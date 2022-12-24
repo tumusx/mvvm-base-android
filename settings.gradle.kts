5f78563b048eb098b3ae7ba2a9a1adb483639a80
murillo <tumusx1@gmail.com>
2022-12-23 22:43:59 -0300

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+enableFeaturePreview("VERSION_CATALOGS")
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("$rootDir/versions.toml"))
+        }
+    }
+}
+include(":app")
+rootProject.name = "Sample Projects Android"
